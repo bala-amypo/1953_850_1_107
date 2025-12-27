91557bd6a778ef7eb724873ca333833df54fd226
bala-amypo <bala@amypo.in>
2025-12-27 01:57:52 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ParcelServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ParcelServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.demo.service.impl;
 import com.example.demo.model.Parcel;
 import com.example.demo.repository.ParcelRepository;
 import com.example.demo.service.ParcelService;
+import org.springframework.stereotype.Service;
+
 
 @Service
 public class ParcelServiceImpl implements ParcelService {
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.demo.service.impl;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserServiceImpl implements UserService {
